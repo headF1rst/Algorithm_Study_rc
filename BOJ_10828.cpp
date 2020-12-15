945358466ebbb2e6b14e5b9f2726dececfc26ed8
headF1rst <55884834+headF1rst@users.noreply.github.com>
2020-12-16 02:49:08 +0900

    Create BOJ_10828.cpp
    20/12/16
    정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
    
    명령은 총 다섯 가지이다.
    
    push X: 정수 X를 스택에 넣는 연산이다.
    pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
    size: 스택에 들어있는 정수의 개수를 출력한다.
    empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
    top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
    입력
    첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
    
    출력
    출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.

---
+++ BOJ_10828.cpp
@@ -0,0 +1,100 @@
+/* No. 10828 스택 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Stack
+{
+private:
+    int *stack;
+    int top;
+    int capacity;
+public:
+    Stack() 
+    {
+        top=-1; 
+        capacity = 4;
+        stack = new int[capacity];
+    }
+    void Push(int &x);
+    int Pop();
+    int isEmpty();
+    int showTop();
+    int showSize();
+};
+
+void Stack::Push(int &x)
+{
+    if(top==capacity-1) //스택이 가득 차있을 경우
+    {
+        capacity*=2;
+        int *temp = new int[capacity]; //두배 더 큰 배열 temp
+        for(int i=0; i<=top; i++) temp[i]=stack[i]; 
+        stack=temp; //stack이 이제 더 큰 배열 temp를 가리킴
+        delete [] temp; //temp가 더이상 temp를 가리키지 않음
+    }
+    stack[++top]=x; //top을 한칸 올린후에 그 위치에 x를 삽입
+}
+
+int Stack::Pop()
+{
+    int x;
+    if(top==-1) return -1; //스택이 비어서 삭제할 값이 없는경우 널값을 반환
+
+    x=stack[top--]; //x에 스택의 top이 가리키는 값을 삽입한후 top을 한칸 내림
+    return x;
+}
+
+int Stack::isEmpty()
+{
+    if(top==-1) return 1; //스택이 비었으면 1반환
+    else return 0; //아니면 0반환 
+}
+
+int Stack::showTop()
+{
+    if(top==-1) return -1;
+    return stack[top];
+}
+
+int Stack::showSize()
+{
+    return top+1;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    Stack s;
+    string inst; //명령어 
+    int num_inst; //명령어의 개수 
+    int num;
+    
+    cin>>num_inst;
+    while(num_inst--)
+    {
+        cin>>inst;
+        if(inst=="push")
+        {
+            cin>>num;
+            s.Push(num);
+        }
+        else if(inst=="pop")
+        {
+            cout<<s.Pop()<<'\n';
+        }
+        else if(inst=="top")
+        {
+            cout<<s.showTop()<<'\n';
+        }
+        else if(inst=="size")
+        {
+            cout<<s.showSize()<<'\n';
+        }
+        else if(inst=="empty")
+        {
+            cout<<s.isEmpty()<<'\n';
+        }
+    }
+    return 0;
+}
