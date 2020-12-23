203f1e91054247157ff4d9271686922dfccd8c0f
headF1rst <55884834+headF1rst@users.noreply.github.com>
2020-12-23 16:21:56 +0900

    Create BOJ_1448.cpp
    삼각형 만들기 
    
    문제
    세준이는 N개의 빨대를 가지고 있다. N개의 빨대 중에 3개의 빨대를 선택했을 때, 이 빨대로 삼각형을 만들 수 있다면, 세 변의 길이의 합의 최댓값을 구하고 싶다.
    
    입력
    첫째 줄에 빨대의 개수 N이 주어진다. N은 3보다 크거나 같고, 1,000,000보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 빨대의 길이가 한 줄에 하나씩 주어진다. 빨대의 길이는 1,000,000보다 작거나 같다.
    
    출력
    첫째 줄에 삼각형 세 변의 길이의 합의 최댓값을 출력한다. 만약 삼각형을 만들 수 없으면 -1을 출력한다.

---
+++ BOJ_1448.cpp
@@ -0,0 +1,29 @@
+/* NO.1539 이진 검색 트리 */
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n,sum;
+    int *length;
+    sum=-1;
+    cin>>n;
+    length=new int[n];
+
+    for(int i=0; i<n; i++) cin>>length[i];
+    
+    sort(length, length+n); //입력받은 수들을 오름차순으로 정렬
+
+    for(int i=n-1; i>=2; i--)
+    {
+        if(length[i]<length[i-1]+length[i-2])
+        {
+            sum+=length[i]+length[i-1]+length[i-2]+1;
+            break;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
